8fdeb8eec18393c820b51e3b6794fb4725597417
Maxence <Maxence.millescamps.macarty@gmail.com>
2018-05-22 16:17:55 +0200

    init storybook

---
+++ src/stories/index.js
@@ -0,0 +1,12 @@
+// stories/index.js
+import { storiesOf } from '@storybook/vue'
+// Components
+import MyButton from '../components/Button.vue'
+
+// Stories: Book Component
+
+storiesOf('button', module)
+  .add('active', () => ({
+    components: { MyButton },
+    template: '<my-button />'
+  }))
