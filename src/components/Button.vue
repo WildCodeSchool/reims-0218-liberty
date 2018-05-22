e98219c82bff5802213979728ae2cc5a9d52cb2f
Aurelie Bayre <a.bayre@gmail.com>
2018-05-22 16:18:13 +0200

    feat: Add example for storybook

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>{{this.text}}</button>
+</template>
+
+<script>
+export default {
+ name: 'MyButton',
+ data: function() {
+   return {
+     text:"je suis un bouton"
+   }
+
+ }
+}
+</script>
