83b93fe5ae2453f5ae40b28ff8febe851babe982
Aurelie Bayre <a.bayre@gmail.com>
2018-05-22 17:44:42 +0200

    feat: add filter button

---
+++ src/components/FilterButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="rounded">{{this.text}}</button>
+</template>
+
+<script>
+export default {
+ name: 'FiltreBouton',
+ data: function() {
+   return {
+     text:"Filtre mots-clés"
+   }
+
+ }
+}
+</script>
+<style>
+.rounded {
+  border-radius: 10px;
+}
+</style>
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,11 +2,16 @@
 import { storiesOf } from '@storybook/vue'
 // Components
 import MyButton from '../components/Button.vue'
-
+import FiltreBouton from '../components/FilterButton.vue'
 // Stories: Book Component
 
 storiesOf('button', module)
-  .add('active', () => ({
+  .add('de base', () => ({
     components: { MyButton },
-    template: '<my-button>coucou</my-button>'
+    template: '<my-button></my-button>'
   }))
+  .add('bouton qui filtre les mots-clés', () => ({
+    components: { FiltreBouton },
+    template: '<filtre-bouton />'
+  }))
+
