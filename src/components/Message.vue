ad9e11ecc2008416d3019e81fc244ad891ee2005
Unknown <florian.hourlier@hotmail.fr>
2018-05-22 21:14:35 +0200

    Message component draft
    Who likes Elsa ?

---
+++ src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <p>{ props.name } - @{ props.shortName } - { props.date }</p>
+      <img src="{ props.avatar }" alt=""/>
+      <p>{ props.message }</p>
+      <img src="{ props.cover }"/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: "Madame Soleil",
+        shortName: "MadameSoleil",
+        date: "2018-01-28",
+        avatar: "http://cache.magicmaman.com/data/photo/w800_c18/137/elsa-home.jpg",
+        message: "Il fait un froid de coin-coin !",
+        cover: "http://cache.magicmaman.com/data/photo/w800_c18/137/elsa-home.jpg",
+    }
+}
+</script>
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,6 +2,7 @@
 import { storiesOf } from '@storybook/vue'
 // Components
 import MyButton from '../components/Button.vue'
+import PanelMessage from '../components/Message.vue'
 
 // Stories: Book Component
 
@@ -9,4 +10,9 @@ storiesOf('button', module)
   .add('active', () => ({
     components: { MyButton },
     template: '<my-button>coucou</my-button>'
+  }))
+storiesOf('message', module)
+  .add('panel', () => ({
+    components: { PanelMessage },
+    template: '<message><div><p>Madame Soleil - @MadameSoleil - 18-05-2017</p><img style="width: 100px; height: 100px; border-radius: 50%;" src="http://cache.magicmaman.com/data/photo/w800_c18/137/elsa-home.jpg" alt=""/><p>Il fait un froid de coin-coin !</p><img src="http://cache.magicmaman.com/data/photo/w800_c18/137/elsa-home.jpg"/></div></message>'
   }))
