17c9eb0b6ba9491a25b74c27f993e724707b164c
Aurelie Bayre <a.bayre@gmail.com>
2018-05-23 09:06:57 +0200

    feat: Correct component structure

--- src/components/FilterButton.vue
+++ src/components/FilterButton.vue
@@ -2,22 +2,12 @@ import Vue from 'vue'
 Vue.use(Vuetify)
 
 <template>
-  <v-app>
-    <v-btn outline color="grey" class="rounded">
-      {{this.text}}
-      </v-btn>
-      <span class="group pa-2">
-        <v-icon class="icones">mouse</v-icon>
-      </span>
-      <span class="group pa-2 icones">
-        <v-icon color="black">home</v-icon>
-        <v-icon dark left>event</v-icon>
-        <v-icon>info</v-icon>
-      </span>
-      <v-icon>cloud</v-icon>
-    <i class="material-icons">label</i>
-    <i class="material-icons">accessible</i>
-  </v-app>
+  <v-btn outline color="grey" class="rounded">
+    <span class="group pa-2">
+      <v-icon>label</v-icon>
+    </span>
+    {{this.text}}
+  </v-btn>
 </template>
 
 <script>
@@ -30,15 +20,11 @@ export default {
   }
 }
 </script>
+
 <style>
-@import url('https://fonts.googleapis.com/css?family=Material+Icons');
-.rounded {
-  border-radius: 5px;
-  font-family: "Material Icons";
-}
+  @import url('https://fonts.googleapis.com/css?family=Material+Icons');
 
-.icones {
-  font-family: "Material Icons";
+  .rounded {
+    border-radius: 5px;
 }
-
 </style>
