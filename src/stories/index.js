945efc0be45a07bf1edf23badbc1a2ac8faf33e5
Unknown <florian.hourlier@hotmail.fr>
2018-05-23 14:12:32 +0200

    Cloud of words component
    With styling according to wireframe

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,11 +1,12 @@
 import { storiesOf } from '@storybook/vue'
 import MyButton from '../components/Button.vue'
 import MyMessage from '../components/Message.vue'
+import MyCloud from '../components/WordCloud.vue'
 import Vuetify from 'vuetify'
 import Vue from 'vue'
 Vue.use(Vuetify)
 
-storiesOf('MyButton', module)
+storiesOf('Machin truc', module)
   .add('story as a component', () => ({
     components: { MyButton },
     template: '<my-button :rounded="true">rounded</my-button>'
@@ -13,4 +14,8 @@ storiesOf('MyButton', module)
   .add('bloc de message', () => ({
     components: { MyMessage },
     template: '<my-message>memessage</my-message>'
+  }))
+  .add('nuage de mots-clés', () => ({
+    components: { MyCloud },
+    template: '<my-cloud>nuage de mots-clés</my-cloud>'
   }))
