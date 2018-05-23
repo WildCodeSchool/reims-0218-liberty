5bf418a507c3c9f9fe426f0a374701895c856b4c
Unknown <florian.hourlier@hotmail.fr>
2018-05-23 14:41:17 +0200

    Shortkey definition component
    TODO: Write script

---
+++ src/components/ShortKey.vue
@@ -0,0 +1,43 @@
+<template>
+    <span>
+        Définir la touche raccourci:
+        <input type="text" readonly id="definedShortKey" value="">
+        <button>C'est prêt !</button><v-icon>mouse</v-icon>
+    </span>
+</template>
+
+<script>
+
+// Script d'attribution de touche clavier
+// Recours à l'événement focus, appliqué à l'input
+// Ecouter l'input fait
+// Avoir la possibilité de le changer à tout moment
+
+export default {
+    name: 'MaterialIcons'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Material+Icons');
+span {
+    width: 100%;
+}
+
+input {
+    border: 1px solid black;
+    background-color: #999;
+    margin: 0 0.5em;
+    width: 15%;
+}
+
+button {
+    min-width: 10%;
+    border-radius: 50%;
+    background-color: darkslategrey;
+    color: white;
+    margin: 1em;
+    padding: 1em
+}
+
+</style>
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,20 +2,25 @@ import { storiesOf } from '@storybook/vue'
 import MyButton from '../components/Button.vue'
 import MyMessage from '../components/Message.vue'
 import MyCloud from '../components/WordCloud.vue'
+import MyShortKey from '../components/ShortKey.vue'
 import Vuetify from 'vuetify'
 import Vue from 'vue'
 Vue.use(Vuetify)
 
-storiesOf('Machin truc', module)
-  .add('story as a component', () => ({
+storiesOf('Composants', module)
+  .add('Juste pour test', () => ({
     components: { MyButton },
     template: '<my-button :rounded="true">rounded</my-button>'
   }))
-  .add('bloc de message', () => ({
+  .add('Bloc de message', () => ({
     components: { MyMessage },
     template: '<my-message>memessage</my-message>'
   }))
-  .add('nuage de mots-clés', () => ({
+  .add('Nuage de mots-clés', () => ({
     components: { MyCloud },
     template: '<my-cloud>nuage de mots-clés</my-cloud>'
+  }))
+  .add('Attribution du raccourci clavier', () => ({
+    components: { MyShortKey },
+    template: '<my-short-key></my-short-key>'
   }))
