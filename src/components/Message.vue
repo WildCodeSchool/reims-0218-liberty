526bda88c4fcba8f2de33e7721d0410e9de5913f
Unknown <florian.hourlier@hotmail.fr>
2018-05-23 11:07:33 +0200

    Message panel works in Storybook

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,9 +1,31 @@
 <template>
-    <div>
-      <p>{ props.name } - @{ props.shortName } - { props.date }</p>
-      <img src="{ props.avatar }" alt=""/>
-      <p>{ props.message }</p>
-      <img src="{ props.cover }"/>
+    <div id="card">
+        <v-content>
+            <v-container text-xs-center>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <p id="author">Madame Soleil - @MadameSoleil - 28 janv. 2018</p>
+                    </v-flex>
+                </v-layout>
+                <v-layout row id="bodyMessage">
+                    <v-flex hidden-xs-only md2>
+                        <v-avatar :size="64"><img id="avatar" src="http://cache.magicmaman.com/data/photo/w800_c18/137/elsa-home.jpg"></v-avatar>
+                    </v-flex>
+                    <v-flex xs12 md10>
+                        <p id="message">Quand on prend de la hauteur, tout semble insignifiant ! La tristesse, l'angoisse et la peur, m'ont quitté depuis longtemps ! Je veux voir ce que je peux faire, de cette magie pleine de mystères. Le bien, le mal, je dis tant pis ! Tant pis !</p>
+                    </v-flex>
+                </v-layout>
+                <v-layout row wrap>
+                    <v-flex>
+                        <img id="cover" src="http://cache.magicmaman.com/data/photo/w800_c18/137/elsa-home.jpg">
+                        <p><input type="text" placeholder="Associez ce message à un ou plusieurs mots-clés..."></p>
+                        <div id="actionButton">
+                            <button>Bouton 1</button><button>Bouton 2</button><button>Bouton 3</button><button>Bouton 4</button><button>Bouton 5</button>
+                        </div>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
     </div>
 </template>
 
@@ -19,3 +41,45 @@ export default {
     }
 }
 </script>
+
+<style>
+#card {
+            width: 50%;
+            min-width: 300px;
+            border-radius: 5%;
+            border: 2px solid purple;
+            padding: 0;
+            margin: 1em;
+        }
+
+        #avatar {
+            border-radius: 50%;
+            float: left;
+            margin: 1em;
+        }
+
+        #author {
+            text-align: left;
+            color: #999;
+        }
+
+        #cover {
+            width: 100%;
+            height: auto;
+            padding: 0;
+        }
+
+        #message {
+            text-align: left;
+            margin: 0.5em;
+        }
+
+        input {
+            width: 100%;
+        }
+
+        button {
+            margin: 0 5%;
+            width: 10%;
+        }
+</style>
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,18 +1,13 @@
-// stories/index.js
 import { storiesOf } from '@storybook/vue'
-// Components
 import MyButton from '../components/Button.vue'
-import PanelMessage from '../components/Message.vue'
+import MyMessage from '../components/Message.vue'
 
-// Stories: Book Component
-
-storiesOf('button', module)
-  .add('active', () => ({
+storiesOf('MyButton', module)
+  .add('story as a component', () => ({
     components: { MyButton },
-    template: '<my-button>coucou</my-button>'
+    template: '<my-button :rounded="true">rounded</my-button>'
   }))
-storiesOf('message', module)
-  .add('panel', () => ({
-    components: { PanelMessage },
-    template: '<message><div><p>Madame Soleil - @MadameSoleil - 18-05-2017</p><img style="width: 100px; height: 100px; border-radius: 50%;" src="http://cache.magicmaman.com/data/photo/w800_c18/137/elsa-home.jpg" alt=""/><p>Il fait un froid de coin-coin !</p><img src="http://cache.magicmaman.com/data/photo/w800_c18/137/elsa-home.jpg"/></div></message>'
+  .add('bloc de message', () => ({
+    components: { MyMessage },
+    template: '<my-message>memessage</my-message>'
   }))
