9ad9df0436c64edc48653ad8b7a3d6d06b1a8e5b
Aurelie Bayre <a.bayre@gmail.com>
2018-05-24 11:30:08 +0200

    feat: trying to fix the space between cas and the rest

--- src/components/LabelKeywordsChosen.vue
+++ src/components/LabelKeywordsChosen.vue
@@ -1,45 +1,54 @@
 <template>
-  <v-toolbar>
-    <v-chip class="blueChip" d-inline-block>
-      1
-    </v-chip>
-    <v-chip class="blueChip">
-      Nuage de mots clés
-    </v-chip>
-     <v-switch color="cyan"
-      :label="`Switch 1: ${switch1.toString()}`"
-      v-model="switch1"
-    ></v-switch>
-    <v-icon color="cyan">cast</v-icon>
-    <!-- le vrai: cast-connected -->
-  </v-toolbar>
+
+    <v-toolbar>
+
+      <v-chip class="blueChip">
+        1
+      </v-chip>
+      <v-chip class="blueChip">
+        Nuage de mots clés
+      </v-chip>
+      <v-switch color="cyan"
+        :label="`Switch 1: ${switch1.toString()}`"
+        v-model="switch1"
+      ></v-switch>
+      <v-icon color="cyan">cast</v-icon>
+      <!-- le vrai: cast-connected -->
+
+    </v-toolbar>
+
 </template>
 
 <script>
-  export default {
-    name: "LabelKeywordsChosen",
-    data: function() {
-      return {
-        chipColor: "#102C60",
-        checkbox: true,
-        radioGroup: 1,
-        switch1: true
-      }
-    }
-  };
+export default {
+  name: "LabelKeywordsChosen",
+  data: function() {
+    return {
+      chipColor: "#102C60",
+      checkbox: true,
+      radioGroup: 1,
+      switch1: true
+    };
+  }
+};
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css?family=Material+Icons");
 
-.application--wrap .blueChip {
+.blueChip {
   background-color: rgb(15, 64, 92);
   color: white;
-  font-weight:800;
+  font-weight: 800;
   width: fit-content;
 }
 
-.application--wrap label {
-  display: none;
+input {
+  width: fit-content;
+}
+
+.input-group.input-group--selection-controls.switch label {
+    padding-left: 14px;
+    display: none;
 }
 </style>
