22e4f61cce52fb2b018f48661b6d054b53e1e91b
Aurelie Bayre <a.bayre@gmail.com>
2018-05-25 14:36:54 +0200

    fix: replace with chip

--- src/components/WordCloud.vue
+++ src/components/WordCloud.vue
@@ -1,29 +1,29 @@
 <template>
     <div id="wordCloud">
-        <span class="word">Chemise</span>
-        <span class="word">Silence</span>
-        <span class="word">Sommeil</span>
-        <span class="word">Football</span>
-        <span class="word">Éléphant</span>
-        <span class="word">Renne</span>
-        <span class="word">Glauque</span>
-        <span class="word">Radin</span>
-        <span class="word">Ana</span>
-        <span class="word">Horreur</span>
-        <span class="word">Chat</span>
-        <span class="word">Sombrero</span>
-        <span class="word">Arnaque</span>
-        <span class="word">Brocoli</span>
-        <span class="word">Chrome</span>
-        <span class="word">Décontenancé</span>
-        <span class="word">Herpès</span>
-        <span class="word">Dracula</span>
-        <span class="word">Saumon</span>
-        <span class="word">Josiane</span>
-        <span class="word">Minikeums</span>
-        <span class="word">Ulysse 31</span>
-        <span class="word">Variable</span>
-        <span class="word">Fatalité</span>
+        <v-chip class="word">Chemise</v-chip>
+        <v-chip class="word">Silence</v-chip>
+        <v-chip class="word">Sommeil</v-chip>
+        <v-chip class="word">Football</v-chip>
+        <v-chip class="word">Éléphant</v-chip>
+        <v-chip class="word">Renne</v-chip>
+        <v-chip class="word">Glauque</v-chip>
+        <v-chip class="word">Radin</v-chip>
+        <v-chip class="word">Ana</v-chip>
+        <v-chip class="word">Horreur</v-chip>
+        <v-chip class="word">Chat</v-chip>
+        <v-chip class="word">Sombrero</v-chip>
+        <v-chip class="word">Arnaque</v-chip>
+        <v-chip class="word">Brocoli</v-chip>
+        <v-chip class="word">Chrome</v-chip>
+        <v-chip class="word">Décontenancé</v-chip>
+        <v-chip class="word">Herpès</v-chip>
+        <v-chip class="word">Dracula</v-chip>
+        <v-chip class="word">Saumon</v-chip>
+        <v-chip class="word">Josiane</v-chip>
+        <v-chip class="word">Minikeums</v-chip>
+        <v-chip class="word">Ulysse 31</v-chip>
+        <v-chip class="word">Variable</v-chip>
+        <v-chip class="word">Fatalité</v-chip>
     </div>
 </template>
 
@@ -39,7 +39,8 @@
     display: inline-block;
     border: 1px solid #777;
     padding: 0.25em 0.75em;
-    border-radius: 7px;
+    font-size: 18px;
+    /* border-radius: 7px; */
     margin: 0.5em 1em;
 }
 
