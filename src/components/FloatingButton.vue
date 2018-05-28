b715393a74c3a814ce553ae2f0269ba2815259b1
Maxence <Maxence.millescamps.macarty@gmail.com>
2018-05-28 14:29:52 +0200

    modif src/components/FloatingButton.vue ("<v-app>" is neened)

--- src/components/FloatingButton.vue
+++ src/components/FloatingButton.vue
@@ -1,60 +1,58 @@
 <template>
-  <div id="app">
-    <v-app id="inspire">
-      <v-card height="300" id="create">
-        <v-container>
-        <v-speed-dial class="speed-dial speed-dial--right speed-dial--bottom speed-dial--direction-top"
+  <v-app>
+    <v-card height="300" id="create">
+      <v-container>
+      <v-speed-dial class="speed-dial speed-dial--right speed-dial--bottom speed-dial--direction-top"
+      >
+        <v-btn
+          slot="activator"
+          color="blue darken-2"
+          dark
+          fab
         >
-          <v-btn
-            slot="activator"
-            color="blue darken-2"
-            dark
-            fab
-          >
-            <v-icon>account_circle</v-icon>
-            <v-icon>close</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            dark
-            small
-            color="green"
-          >
-            <v-icon>edit</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            dark
-            small
-            color="indigo"
-          >
-            <v-icon>add</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            dark
-            small
-            color="red"
-          >
-            <v-icon>delete</v-icon>
-          </v-btn>
-        </v-speed-dial>
-        </v-container>
-      </v-card>
-    </v-app>
-  </div>
+          <v-icon>account_circle</v-icon>
+          <v-icon>close</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          small
+          color="green"
+        >
+          <v-icon>edit</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          small
+          color="indigo"
+        >
+          <v-icon>add</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          small
+          color="red"
+        >
+          <v-icon>delete</v-icon>
+        </v-btn>
+      </v-speed-dial>
+      </v-container>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: "FloatingButton",
   data: () => ({
-    direction: 'top',
+    direction: "top",
     right: true,
     bottom: true,
-    transition: 'scale-transition'
+    transition: "scale-transition"
   })
-}
+};
 </script>
 
 <style>
