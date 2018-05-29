c05ce3eb6cd341100bf4c2c3a5c538713ddba302
Aurelie Bayre <a.bayre@gmail.com>
2018-05-29 17:44:09 +0200

    fix: clean container around layout slightly better but not there yet

--- src/components/ActivitiesForm.vue
+++ src/components/ActivitiesForm.vue
@@ -1,14 +1,22 @@
 <template>
-  <v-container grid-list-md text-xs-center>
+
+  <!-- <v-container grid-list-md text-xs-center> -->
     <v-layout row wrap>
-      <v-flex xs6>
-        <v-switch v-model="switch1" label="Activité permanente" value="1"></v-switch>
-        <v-switch v-model="switch2" label="Modération" value="2"></v-switch>
-        <v-switch v-model="switch3" label="Autoriser les photos" value="3"></v-switch>
-        <v-switch v-model="switch4" label="Voir le pseudo" value="4"></v-switch>
+      <v-flex xs4>
+        <!-- Weird behaviour, the label is enough when displaying this component in storybook,
+        but it's invisible when I compose the whole page.
+        My solution for now:
+        adding a text after the switch
+        + visibility hidden on the label just in case it reappears in other setups.
+        -->
+        <span class="no-label"><v-switch v-model="switch1" label="Activité permanente" value="1"></v-switch> Activité permanente</span>
+        <span class="no-label"><v-switch v-model="switch2" label="Modération" value="2"></v-switch>Modération</span>
+        <span class="no-label"><v-switch v-model="switch3" label="Autoriser les photos" value="3"></v-switch>Autoriser les photos</span>
+        <span class="no-label"><v-switch v-model="switch4" label="Voir le pseudo" value="4"></v-switch>Voir le pseudo</span>
       </v-flex>
     </v-layout>
-  </v-container>
+  <!-- </v-container> -->
+
 </template>
 
 <script>
@@ -30,9 +38,9 @@ export default {
 </script>
 <style>
 @import url('https://fonts.googleapis.com/css?family=Material+Icons');
-.rounded {
-  border-radius: 5px;
-  font-family: "Material Icons";
+
+.no-class label {
+  display: none;
 }
 
 </style>
