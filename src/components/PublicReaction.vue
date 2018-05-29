5e40075284fe4038b789df367d3cf3bb5a671c0d
Aurelie Bayre <a.bayre@gmail.com>
2018-05-29 22:14:52 +0200

    feat: add v-list etc on public reaction

--- src/components/PublicReaction.vue
+++ src/components/PublicReaction.vue
@@ -1,34 +1,40 @@
 <template>
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
-      <ul id="optionsPublic">
-          <li>
-              Style d'affichage à l'écran
-            <v-radio-group v-model="row" row>
-              <v-radio label="bandeau" value="radio-1" ></v-radio>
-              <v-radio label="pleine page" value="radio-2"></v-radio>
-            </v-radio-group>
-          </li>
-          <li class="label-hidden">
-            Temps d'affichage du message :
-            <v-text-field
-              label="secondes"
-              placeholder="10"
-              value="10"
-              suffix="secondes"
-              box
-            />
-          </li>
-          <li class="label-hidden">
-              <p>Personnalisez la box du message :</p>
-              <v-text-field
-                name="personnalisation"
-                label="Réagissez en direct !"
-                placeholder="Réagissez en direct !"
-                textarea
-            />
-          </li>
-      </ul>
+      <v-list id="optionsPublic">
+        <v-list-tile>
+            Style d'affichage à l'écran
+          <v-radio-group v-model="row" row>
+            <v-radio label="bandeau" value="radio-1" ></v-radio>
+            <v-radio label="pleine page" value="radio-2"></v-radio>
+          </v-radio-group>
+        </v-list-tile>
+        <v-list-tile class="label-hidden">
+          Temps d'affichage du message :
+          <v-text-field
+            label="secondes"
+            placeholder="10"
+            value="10"
+            suffix="secondes"
+            box
+          />
+        </v-list-tile>
+        <v-list-tile class="label-hidden">
+          Personnalisez la box du message :
+        </v-list-tile>
+        <v-spacer></v-spacer>
+        <v-list-tile>
+          <v-list-tile-content>
+          <v-text-field
+              name="personnalisation"
+              label="Réagissez en direct !"
+              placeholder="Réagissez en direct !"
+              textarea
+          />
+        </v-list-tile-content>
+        </v-list-tile>
+
+      </v-list>
     </v-layout>
   </v-container>
 </template>
@@ -48,7 +54,6 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=Material+Icons");
 #optionsPublic {
   text-align: left;
-  line-height: 2em;
 }
 
 .label-hidden label {
