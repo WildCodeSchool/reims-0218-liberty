760455070cf954b526debad4ca559ec53c876de9
Aurelie Bayre <a.bayre@gmail.com>
2018-05-29 06:40:49 +0200

    fix: Center image in message box

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,36 +1,40 @@
 <template>
-  <v-app>
-    <div id="card">
-      <v-content>
-        <v-layout row wrap>
-          <v-flex xs12>
-            <p id="author">Super Nanny Officiel - @SuperNanny - 25 mai 2018</p>
-          </v-flex>
-        </v-layout>
-        <v-layout row id="bodyMessage">
-          <v-flex hidden-xs-only>
-            <v-avatar :size="64"><img id="avatar" src="https://pbs.twimg.com/media/DSEsQf7W0AA5rNd.jpg"></v-avatar>
-          </v-flex>
-          <v-flex xs12 md10>
-            <p id="message">Maxence est comme un enfant de 3 ans. Toujours en train de râler pour un oui ou pour un non. Si je devais vous donner un conseil, ça serait de le priver de kebabs pour le restant de ses jours ; et de le forcer à boire de l'huile de foie de morue ! C'est un ami qui m'a proposé cette idée ! :)</p>
-          </v-flex>
-        </v-layout>
-        <v-layout row wrap>
-          <div class="illustration">
-            <img id="cover" src="http://www.toutelatele.com/IMG/arton54183.jpg">
-          </div>
-          <v-icon>label</v-icon> <v-text-field value="Associez ce message à un ou plusieurs mots-clés"></v-text-field>
-          <span id="actionButton" class="mx-auto">
-            <v-icon>check_box_marked_outline</v-icon>
-            <v-icon>image_plus</v-icon>
-            <v-icon>cast_connected</v-icon>
-            <v-icon>mouse</v-icon> <!-- pencil -->
-            <v-icon>mouse</v-icon> <!-- trash -->
-          </span>
-        </v-layout>
-      </v-content>
-    </div>
-  </v-app>
+    <v-app>
+        <div>
+            <v-flex xs6 id="card">
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <p id="author">Super Nanny Officiel - @SuperNanny - 25 mai 2018</p>
+                    </v-flex>
+                </v-layout>
+                <v-layout row id="bodyMessage">
+                    <v-flex hidden-xs-only>
+                        <v-avatar :size="64"><img id="avatar" src="https://pbs.twimg.com/media/DSEsQf7W0AA5rNd.jpg"></v-avatar>
+                    </v-flex>
+                    <v-flex xs12 md10>
+                        <p id="message">Maxence est comme un enfant de 3 ans. Toujours en train de râler pour un oui ou pour un non. Si je devais vous donner un conseil, ça serait de le priver de kebabs pour le restant de ses jours ; et de le forcer à boire de l'huile de foie de morue ! C'est un ami qui m'a proposé cette idée ! :)</p>
+                    </v-flex>
+                </v-layout>
+                <v-layout row wrap>
+                  <v-flex xs12 justify-center>
+                  <div class="illustration text-xs-center">
+                    <img id="cover" src="http://www.toutelatele.com/IMG/arton54183.jpg">
+                  </div>
+                </v-flex>
+
+                  <v-icon>label</v-icon> <v-text-field value="Associez ce message à un ou plusieurs mots-clés"></v-text-field>
+                  <span id="actionButton" class="mx-auto">
+                      <v-icon>check_box_marked_outline</v-icon>
+                      <v-icon>image_plus</v-icon>
+                      <v-icon>cast_connected</v-icon>
+                      <v-icon>mouse</v-icon> <!-- pencil -->
+                      <v-icon>mouse</v-icon> <!-- trash -->
+                  </span>
+                </v-layout>
+            </v-flex>
+        </div>
+
+    </v-app>
 </template>
 
 <script>
@@ -90,6 +94,7 @@ export default {
   max-width: 100%;
   height: auto;
   padding: 0;
+  margin: auto;
 }
 
 #message {
