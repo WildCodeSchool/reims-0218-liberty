e1ee78f4edb9a95d5c1435aa3c58ebef87e70c4b
Aurelie Bayre <a.bayre@gmail.com>
2018-05-29 17:22:35 +0200

    fix: change labels in switches

--- src/components/ActivitiesForm.vue
+++ src/components/ActivitiesForm.vue
@@ -2,10 +2,10 @@
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
       <v-flex xs6>
-        <v-switch v-model="switch1" label="1" value="1"></v-switch>
-        <v-switch v-model="switch2" label="2" value="2"></v-switch>
-        <v-switch v-model="switch3" label="3" value="3"></v-switch>
-        <v-switch v-model="switch4" label="4" value="4"></v-switch>
+        <v-switch v-model="switch1" label="Activité permanente" value="1"></v-switch>
+        <v-switch v-model="switch2" label="Modération" value="2"></v-switch>
+        <v-switch v-model="switch3" label="Autoriser les photos" value="3"></v-switch>
+        <v-switch v-model="switch4" label="Voir le pseudo" value="4"></v-switch>
       </v-flex>
     </v-layout>
   </v-container>
