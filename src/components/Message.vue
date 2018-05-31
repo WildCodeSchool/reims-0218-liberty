7268d394796cd92128ea2f4a610cad2c90042824
Unknown <florian.hourlier@hotmail.fr>
2018-05-31 12:12:05 +0200

    Messages displayed in 2 columns
    TODO: see how can I ignore vertical spaces

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,36 +1,34 @@
 <template>
-  <div>
-    <v-flex xs6 id="card">
-      <v-layout row wrap>
-        <v-flex xs12>
-          <p id="author">{{ author }} - @{{ arobase }} - {{ date }}</p>
-        </v-flex>
-      </v-layout>
-      <v-layout row id="bodyMessage">
-        <v-flex hidden-xs-only>
-          <v-avatar :size="64"><img id="avatar" v-bind:src="avatar"></v-avatar>
-        </v-flex>
-        <v-flex xs12 md10>
-          <p id="message">{{ message }}</p>
-        </v-flex>
-      </v-layout>
-      <v-layout row wrap>
-        <v-flex xs12 justify-center>
-        <div class="illustration text-xs-center">
-          <div id="cover" :style="{'background-image': 'url(' + this.cover + ')'}"></div>
-        </div>
+  <div id="card">
+    <v-layout row wrap>
+      <v-flex xs12>
+        <p id="author">{{ author }} - @{{ arobase }} - {{ date }}</p>
       </v-flex>
-        <v-icon id="inputIcon">label</v-icon>
-        <v-text-field class="textfield" placeholder="Associez ce message à un ou plusieurs mots-clés"></v-text-field>
-        <span id="actionButton">
-          <v-icon class="icon">check_box_marked_outline</v-icon>
-          <v-icon class="icon">image_plus</v-icon>
-          <v-icon class="icon">cast_connected</v-icon>
-          <v-icon class="icon">mouse</v-icon> <!-- pencil -->
-          <v-icon class="icon">mouse</v-icon> <!-- trash -->
-        </span>
-      </v-layout>
+    </v-layout>
+    <v-layout row id="bodyMessage">
+      <v-flex hidden-xs-only>
+        <v-avatar :size="64"><img id="avatar" v-bind:src="avatar"></v-avatar>
+      </v-flex>
+      <v-flex xs12 md10>
+        <p id="message">{{ message }}</p>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 justify-center>
+      <div class="illustration text-xs-center">
+        <div id="cover" :style="{'background-image': 'url(' + this.cover + ')'}"></div>
+      </div>
     </v-flex>
+      <v-icon id="inputIcon">label</v-icon>
+      <v-text-field class="textfield" placeholder="Associez ce message à un ou plusieurs mots-clés"></v-text-field>
+      <span id="actionButton">
+        <v-icon class="icon">check_box_marked_outline</v-icon>
+        <v-icon class="icon">image_plus</v-icon>
+        <v-icon class="icon">cast_connected</v-icon>
+        <v-icon class="icon">mouse</v-icon> <!-- pencil -->
+        <v-icon class="icon">mouse</v-icon> <!-- trash -->
+      </span>
+    </v-layout>
   </div>
 </template>
 
@@ -78,12 +76,14 @@ export default {
 @import url('https://fonts.googleapis.com/css?family=Material+Icons');
 
 #card {
-  width: 60%;
+  width: 40%;
+  display: inline-block;
   min-width: 300px;
   border-radius: 5%;
   border: 2px solid purple;
   padding: 0;
-  margin: 1em;
+  margin: 24px 5%;
+  page-break-inside: avoid;
 }
 
 #avatar {
