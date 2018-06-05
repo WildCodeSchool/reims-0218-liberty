34d96d69412f814e41e07eb1eb4fdd8412de2eda
Aurelie Bayre <a.bayre@gmail.com>
2018-06-05 12:04:15 +0200

    fix: cfix color of btrunton in tous

--- src/components/Tous.vue
+++ src/components/Tous.vue
@@ -18,17 +18,16 @@ export default {
 
 .rounded {
   border-radius: 5px;
+  color: white;
+  font-family: "Roboto", sans-serif;
   font-size: 10px;
+  width: fit-content;
 }
+
 .gradient {
   background: linear-gradient(45deg, indigo, turquoise 96%);
   font-weight: 800;
   letter-spacing: 0.2rem;
 }
 
-.theme--light .rounded {
-   color: white;
-   width: fit-content;
-   font-family: "Roboto", sans-serif;
-}
 </style>
